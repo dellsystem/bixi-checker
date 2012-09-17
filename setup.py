f6f0be856f91d7ee21d6af38511cc217abb59bdd
dellsystem <ilostwaldo@gmail.com>
2012-09-16 22:48:36 -0400

    Fix errors in setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 setup(
     name='Bixi API Wrapper',
     version='0.0.1',
-    scripts='bin/bixi',
-    packages='bixiapi',
+    scripts=['bin/bixi'],
+    packages=['bixiapi'],
     author='Wendy Liu',
     author_email='ilostwaldo@gmail.com',
     description="A simple wrapper for the bixi.com API. Well, there isn't \
